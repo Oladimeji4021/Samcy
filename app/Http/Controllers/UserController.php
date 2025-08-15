17f0da5d080e0d09ac855186970b19662971e06a
oladimeji402 <olamoney665@gmail.com>
2025-08-15 10:22:37 +0100

    profile update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ use App\Models\User;
 class UserController extends Controller
 {
     //
-     public function me(Request $request)
+    public function me(Request $request)
     {
         return response()->json($request->user(), 200);
     }
@@ -22,37 +22,36 @@ class UserController extends Controller
 
 
     public function update(UserUpdateRequest $request)
-        {
-            $user = $request->user(); 
-            $user->update($request->validated());
+    {
+        $user = $request->user();
+        $user->update($request->validated());
 
-            return response()->json([
+        return response()->json([
             'message' => 'Profile updated successfully',
             'user' => $user
-            ], 200);
-        }
-
-
-public function updateProfilePicture(UpdateProfilePictureRequest $request)
-{
-    $user = $request->user();
-
-    if ($request->hasFile('profile_picture')) {
-        if ($user->profile_picture) {
-            Storage::disk('public')->delete($user->profile_picture);
-        }
-
-        $file = $request->file('profile_picture');
-        $path = $file->store('profile_pictures', 'public');
-
-        $user->profile_picture = $path;
-        $user->save();
+        ], 200);
     }
 
-    return response()->json([
-        'message' => 'Profile picture updated successfully',
-        'user' => $user,
-    ], 200);
-}
 
-       }
+    public function updateProfilePicture(UpdateProfilePictureRequest $request)
+    {
+        $user = $request->user();
+
+        if ($request->hasFile('profile_picture')) {
+            if ($user->profile_picture) {
+                Storage::disk('public')->delete($user->profile_picture);
+            }
+
+            $file = $request->file('profile_picture');
+            $path = $file->store('profile_pictures', 'public');
+
+            $user->profile_picture = $path;
+            $user->save();
+        }
+
+        return response()->json([
+            'message' => 'Profile picture updated successfully',
+            'user' => $user,
+        ], 200);
+    }
+}
